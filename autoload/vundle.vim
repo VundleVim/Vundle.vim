VundleVim/Vundle.vim

commit 55a5ef041c31c47477bba7583991a45d03d3590a

Preserve `rtp` ordered according to Bundle declarations
- fixes https://github.com/gmarik/vundle/issues#issue/3
- TODO: not the most efficient approach

autoload/vundle.vim
@@ -10,8 +10,6 @@ com! -nargs=0       BundleDocs            call vundle#installer#helptags()
 
 com! -nargs=+ -bang BundleSearch  silent  call vundle#scripts#search("<bang>", <q-args>)
 
-if !exists('g:bundles') | let g:bundles = [] | endif
-
 func! vundle#rc()
   let g:bundle_dir = expand('$HOME/.vim/bundle')
   call vundle#config#init()

autoload/vundle/config.vim
@@ -1,14 +1,16 @@
 func! vundle#config#bundle(arg, ...)
   let bundle = extend(s:parse_options(a:000), s:parse_name(a:arg))
   call extend(bundle, copy(s:bundle))
+  call s:rtp_rm_a()
   call add(g:bundles, bundle)
-  call s:rtp_add(bundle.rtpath())
+  call s:rtp_add_a()
   " TODO: fix this: explicit sourcing kills command-T
   " call vundle#config#require(bundle)
 endf
 
 func! vundle#config#init()
-  call filter(g:bundles, 's:rtp_rm(v:val.rtpath())')
+  if !exists('g:bundles') | let g:bundles = [] | endif
+  call s:rtp_rm_a()
   let g:bundles = []
 endf
 
@@ -41,6 +43,14 @@ func! s:parse_name(arg)
     let uri  = 'http://github.com/vim-scripts/'.name.'.git'
   endif
   return {'name': name, 'uri': uri }
+endf
+
+func! s:rtp_rm_a()
+  for b in g:bundles | call s:rtp_rm(b.rtpath()) | endfor
+endf
+
+func! s:rtp_add_a()
+  for b in reverse(copy(g:bundles)) | call s:rtp_add(b.rtpath()) | endfor
 endf
 
 func! s:rtp_rm(dir)

commit 4391eb35170f9d19ba484b3b8176e4222ce819b3

Introduce BundleBind! command to speed up Vim startup
The performance issue was introduced in 55a5ef04.

autoload/vundle.vim
@@ -7,6 +7,9 @@
 com! -nargs=+         Bundle
 \ call vundle#config#bundle(<args>)
 
+com! -nargs=0 -bang BundleBind
+\ call vundle#config#bind()
+
 com! -nargs=? -bang -complete=custom,vundle#scripts#complete BundleInstall
 \ call vundle#installer#new('!' == '<bang>', <q-args>)
 

autoload/vundle/config.vim
@@ -1,8 +1,11 @@
 func! vundle#config#bundle(arg, ...)
   let bundle = vundle#config#init_bundle(a:arg, a:000)
-  call s:rtp_rm_a()
   call add(g:bundles, bundle)
-  call s:rtp_add_a()
+endf
+
+func! vundle#config#bind()
+  call s:rtp_rm_a()
+  call vundle#config#require(g:bundles)
 endf
 
 func! vundle#config#init()

plugin/vundle.vim
@@ -1,0 +1,7 @@
+if exists('g:loaded_vundle') || &cp
+  finish
+endif
+let g:loaded_vundle = 1
+
+" Bind and load all scripts bundles immediately.
+BundleBind!